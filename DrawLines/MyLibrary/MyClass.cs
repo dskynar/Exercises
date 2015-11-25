9c6932f1ed3fe10d18fa1b599c80329997543289
David Skynar <david.skynar@gmail.com>
2015-11-25 09:35:21 +0100

    add nunit test

--- DrawLines/MyLibrary/MyClass.cs
+++ DrawLines/MyLibrary/MyClass.cs
@@ -7,6 +7,17 @@ namespace MyLibrary
 		public MyClass ()
 		{
 		}
+
+		public int Divide (int number1, int number2)
+		{
+			int result;
+			try {
+				result = number1 / number2;
+			} catch (DivideByZeroException) {
+				result = -1;
+			}
+			return result;
+		}
 	}
 }
 
