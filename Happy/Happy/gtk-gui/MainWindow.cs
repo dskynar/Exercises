a2b1df24ae012ea95a10d96799894c5c15478a9e
David Skynar <david.skynar@gmail.com>
2015-11-27 12:37:20 +0100

    Add three fields

--- Happy/Happy/gtk-gui/MainWindow.cs
+++ Happy/Happy/gtk-gui/MainWindow.cs
@@ -3,6 +3,11 @@
 
 public partial class MainWindow
 {
+	private global::Gtk.Frame frame1;
+	private global::Gtk.Alignment GtkAlignment;
+	private global::Gtk.VBox vbox5;
+	private global::Gtk.Label GtkLabel;
+	
 	protected virtual void Build ()
 	{
 		global::Stetic.Gui.Initialize (this);
@@ -10,6 +15,26 @@ public partial class MainWindow
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString ("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
+		// Container child MainWindow.Gtk.Container+ContainerChild
+		this.frame1 = new global::Gtk.Frame ();
+		this.frame1.Name = "frame1";
+		this.frame1.ShadowType = ((global::Gtk.ShadowType)(0));
+		// Container child frame1.Gtk.Container+ContainerChild
+		this.GtkAlignment = new global::Gtk.Alignment (0F, 0F, 1F, 1F);
+		this.GtkAlignment.Name = "GtkAlignment";
+		this.GtkAlignment.LeftPadding = ((uint)(12));
+		// Container child GtkAlignment.Gtk.Container+ContainerChild
+		this.vbox5 = new global::Gtk.VBox ();
+		this.vbox5.Name = "vbox5";
+		this.vbox5.Spacing = 6;
+		this.GtkAlignment.Add (this.vbox5);
+		this.frame1.Add (this.GtkAlignment);
+		this.GtkLabel = new global::Gtk.Label ();
+		this.GtkLabel.Name = "GtkLabel";
+		this.GtkLabel.LabelProp = global::Mono.Unix.Catalog.GetString ("<b>GtkFrame</b>");
+		this.GtkLabel.UseMarkup = true;
+		this.frame1.LabelWidget = this.GtkLabel;
+		this.Add (this.frame1);
 		if ((this.Child != null)) {
 			this.Child.ShowAll ();
 		}
